44cb8833c6e65ad93d6fa81e38b38e21b8bce6b6
kunalvirk <kunal@websilt.com>
2019-03-01 21:28:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-const port = process.env.PORT || '3001';
+const port = process.env.PORT || '5000';
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
 })
